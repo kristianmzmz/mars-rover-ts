52ea51867024722bd15716297d0d022496f7b465
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-14 10:43:24 +0200

    RED: Be able to create a Mars Rover instance

--- typescript/tests/unit/kata-example.spec.ts
+++ typescript/tests/unit/kata-example.spec.ts
@@ -1,5 +1,6 @@
-describe('kata-setup', () => {
-    it('example test', () => {
-        expect(true).toBeTruthy();
+describe('Mars Rover', () => {
+    it('should create the mars rover', () => {
+        let rover = new MarsRover();
+        expect(rover).toBeInstanceOf(MarsRover);
     });
 });
