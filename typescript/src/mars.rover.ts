73f81bbb7b3878a687c4bb21296b84e1a609b1e3
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-14 11:05:17 +0200

    Refactor: Extract position Y to be able to change it independently

--- typescript/src/mars.rover.ts
+++ typescript/src/mars.rover.ts
@@ -1,7 +1,11 @@
 export class MarsRover {
     execute(command: string): string {
-        if(command == "M") return "0,1,N"
-        if(command == "MM") return "0,2,N"
-        return "0,0,N"
+        let positionY = 0;
+
+        command.split("").forEach((actualCommand) => {
+            if(actualCommand == "M") positionY++
+        })
+
+        return `0,${positionY},N`
     }
 }
