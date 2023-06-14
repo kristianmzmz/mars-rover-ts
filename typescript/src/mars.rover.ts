2de1894e82d6766eededb304fc36a76b59f0bd7f
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-14 11:56:57 +0200

    Refactor: Extract Y position to a class property

--- typescript/src/mars.rover.ts
+++ typescript/src/mars.rover.ts
@@ -3,21 +3,21 @@ export const ROTATE_RIGHT = "R";
 
 export class MarsRover {
     private readonly plateauSize = 10;
+    private positionY = 0;
 
     execute(command: string): string {
-        let positionY = 0;
         let direction = `N`;
 
         command.split("").forEach((actualCommand) => {
-            if (this.isAMovementCommand(actualCommand)) positionY++
+            if (this.isAMovementCommand(actualCommand)) this.positionY++
             if (command == ROTATE_RIGHT) direction = "E"
         })
 
-        return `0,${(this.wrapAround(positionY))},${direction}`
+        return `0,${(this.wrapAround())},${direction}`
     }
 
-    private wrapAround(positionY: number): number {
-        return positionY % this.plateauSize;
+    private wrapAround(): number {
+        return this.positionY % this.plateauSize;
     }
 
     private isAMovementCommand(actualCommand: string): boolean {
