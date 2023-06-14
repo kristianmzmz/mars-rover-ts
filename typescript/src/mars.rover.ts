571652c4e900739186427a2fdabda3da716a8afa
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-14 12:13:14 +0200

    Refactor: extract direction to a field of the class

--- typescript/src/mars.rover.ts
+++ typescript/src/mars.rover.ts
@@ -1,25 +1,29 @@
 export const MOVE = "M";
 export const ROTATE_RIGHT = "R";
 
+const NORTH = 'N';
+const EAST = 'E';
+const SOUTH = 'S';
+
 export class MarsRover {
     private readonly plateauSize = 10;
     private positionY = 0;
 
-    execute(command: string): string {
-        let direction = `N`;
+    private direction: string= NORTH;
 
+    execute(command: string): string {
         command.split("").forEach((actualCommand) => {
             if (this.isAMovementCommand(actualCommand)) this.positionY++
             if (actualCommand == ROTATE_RIGHT) {
-                if(direction === 'N') {
-                    direction = 'E'
-                } else if(direction == 'E'){
-                    direction = 'S'
+                if (this.direction === NORTH) {
+                    this.direction = EAST
+                } else if (this.direction == EAST) {
+                    this.direction = SOUTH
                 }
             }
         })
 
-        return `0,${(this.wrapAround())},${direction}`
+        return `0,${(this.wrapAround())},${(this.direction)}`
     }
 
     private wrapAround(): number {
