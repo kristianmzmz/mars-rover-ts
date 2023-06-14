44f8f850f2f326545a492598b4cf5f83f110d2db
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-14 10:59:26 +0200

    Green: Move two positions when facing North

--- typescript/src/mars.rover.ts
+++ typescript/src/mars.rover.ts
@@ -1,6 +1,7 @@
 export class MarsRover {
     execute(command: string): string {
         if(command == "M") return "0,1,N"
+        if(command == "MM") return "0,2,N"
         return "0,0,N"
     }
 }
