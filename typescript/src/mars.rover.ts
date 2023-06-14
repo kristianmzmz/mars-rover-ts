0b5196ed27b30751dedb603c2a71e0895906db3a
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-14 11:18:37 +0200

    Refactor: Extract Movement logic

--- typescript/src/mars.rover.ts
+++ typescript/src/mars.rover.ts
@@ -1,11 +1,17 @@
+const MOVE = "M";
+
 export class MarsRover {
     execute(command: string): string {
         let positionY = 0;
 
         command.split("").forEach((actualCommand) => {
-            if(actualCommand == "M") positionY++
+            if(this.isAMovementCommand(actualCommand)) positionY++
         })
 
         return `0,${positionY % 10},N`
+    }
+
+    private isAMovementCommand(actualCommand: string) {
+        return actualCommand == MOVE;
     }
 }
